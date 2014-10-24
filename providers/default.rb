c990964e1b1d4f14962daf785c541402ec8d76c5
Chris Aumann <me@chr4.org>
2014-10-24 20:10:22 +0200

    Revert "Remove force_unlink workaround"
    The fix is not released before chef-12.0.0

--- providers/default.rb
+++ providers/default.rb
@@ -36,7 +36,10 @@ action :create do
       # Remove file if it is not a regular file (e.g. a symlink)
       # This is useful, as sometimes the backup stored in the file "original"
       # is symlinked to "tail"
-      force_unlink true
+      #
+      # Due to a bug in chef (fixed with https://github.com/opscode/chef/pull/1383, but not released yet),
+      # we need to check whether the file actually exists before setting force_unlink
+      force_unlink true if ::File.exist?("/etc/resolvconf/resolv.conf.d/#{name}")
     end
 
     new_resource.updated_by_last_action(true) if r.updated_by_last_action?
